c3e6d68f51a93408fdc9cfc45ec4b08c616f5081
ro_k_ <ro_k_sana@mail.ru>
2024-07-02 16:40:17 +0500

    DZ_7_DRF. 24.2 Сериализаторы. Задания 1, 2

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from users.models import User
+from users.models import User, Payments
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email',)
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date_payment', 'course_paid', 'payment_method', )
+    list_filter = ('user','course_paid',)
+    search_fields = ('user','course_paid','payment_method',)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import LearningCourse
+
 NULLABLE = {'blank': True, 'null': True}
+
 
 class User(AbstractUser):
     username = None
@@ -12,3 +15,19 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Payments(models.Model):
+    PAYMENT_METHOD = (('Cash', 'Наличные'), ('Transfer', 'Перевод на счет'))
+    objects = None
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    date_payment = models.DateTimeField(auto_now=True, verbose_name='Дата оплаты')
+    course_paid = models.ForeignKey(LearningCourse, on_delete=models.CASCADE, verbose_name='Оплаченный курс')
+    payment_method = models.CharField(max_length=15, choices=PAYMENT_METHOD, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} {self.date_payment} {self.course_paid} {self.payment_method}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
