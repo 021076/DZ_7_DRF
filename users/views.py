08d0e0572483852706219b124a3f48ad1b758a7c
ro_k_ <ro_k_sana@mail.ru>
2024-07-04 18:57:57 +0500

    DZ_7_DRF. 25.1 Права доступа в DRF. доработки

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
-from users.models import Payments
-from users.serializers import PaymentsSerializer
+from rest_framework.permissions import IsAuthenticated
+
+from users.models import Payments, User
+from users.serializers import PaymentsSerializer, UserSerializer
 
 
 class PaymentsListAPIView(generics.ListAPIView):
@@ -11,3 +13,30 @@ class PaymentsListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('course_paid', 'payment_method',)
     ordering_fields = ('date_payment',)
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
