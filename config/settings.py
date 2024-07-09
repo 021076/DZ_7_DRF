0ea954961d8fb69fd39fca67710e3668397679ef
ro_k_ <ro_k_sana@mail.ru>
2024-07-09 23:34:48 +0500

    DZ_7_DRF. 26.2. Celery. Задание 3

--- config/settings.py
+++ config/settings.py
@@ -188,6 +188,13 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CELERY_BEAT_SCHEDULE = {
+    "check_activity_user": {
+        "task": "users.tasks.check_activity_user",
+        "schedule": timedelta(seconds=10),
+    },
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
